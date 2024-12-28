0716d32aa8b1fb3dfe30af573659107568220f78
Stella R <stella.rovazzi@gmail.com>
2024-12-28 01:09:13 -0500

    we've been grinding that home page

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,10 +1,16 @@
-// Wait for the page to load fully
+document.body.style.height = "0vh"; // Set a fixed height of the viewport
+
+//on window load
 window.addEventListener('load', function() {
-    // Select the element with the class "slide-in"
+  $("#navbar-container").load("../html/navbar.html", function() {
+
+    // Once the navbar is loaded, trigger animations
     var name = document.querySelector('.name');
     var subtitle = document.querySelector('.subtitle');
-    
-    // Add the "visible" class to trigger the slide-in animation
+
+    // Check if the elements exist before applying the visible class
     name.classList.add('visible');
     subtitle.classList.add('visible');
+    
   });
+});
