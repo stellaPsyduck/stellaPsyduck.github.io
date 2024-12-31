e43c8ef579c917af0df8c69b423c08445debc9e6
Stella R <stella.rovazzi@gmail.com>
2024-12-31 04:36:54 -0500

    finishing up resume page (could edit education)

--- javascript/resume.js
+++ javascript/resume.js
@@ -1,3 +1,7 @@
 window.addEventListener('load', function() {
     $("#navbar-container").load("../html/navbar.html");
 });
+
+document.getElementById('resume-button').addEventListener('click', function() {
+    window.open('../images/W24_Resume.pdf', '_blank');
+  });
